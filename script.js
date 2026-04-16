f595491e28a4ba3c811e83ab9196397c80fdddf3
verifier-ksa <cardtub@gmail.com>
2026-04-17 02:04:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,20 +18,19 @@ async function verifyCertificate() {
         document.getElementById("result-section").classList.remove("hidden");
 
         document.getElementById("result-data").innerHTML = `
-            <div class="data-item"><span class="label">Deliverable Id :</span>${found.id}</div>
-            <div class="data-item"><span class="label">Published on :</span>${found.published}</div>
-            <div class="data-item"><span class="label">QR Code Status :</span><span style="color:green;">Validated</span></div>
-            <div class="data-item"><span class="label">NAME :</span>${found.name}</div>
-            <div class="data-item"><span class="label">ID :</span>${found.user_id}</div>
-            <div class="data-item"><span class="label">ISSUED ON :</span>${found.published}</div>
-            <div class="data-item"><span class="label">VALID UNTIL :</span>${found.valid_until}</div>
-            <div class="data-item"><span class="label">TYPE :</span>${found.type}</div>
-            <div class="data-item"><span class="label">MODEL :</span>${found.model}</div>
-            <div class="data-item"><span class="label">COMPANY :</span>${found.company}</div>
-            <div class="data-item"><span class="label">TRAINING LOCATION :</span>${found.location}</div>
-            <div class="data-item"><span class="label">TRAINER :</span>${found.trainer}</div>
+            <div class="data-item"><span class="label">Deliverable Id :</span> ${found.id}</div>
+            <div class="data-item"><span class="label">Published on :</span> ${found.published}</div>
+            <div class="data-item"><span class="label">QR Code Status :</span> <span style="color:green;">Validated</span></div>
+            <div class="data-item"><span class="label">NAME :</span> ${found.name}</div>
+            <div class="data-item"><span class="label">ID :</span> ${found.user_id}</div>
+            <div class="data-item"><span class="label">ISSUED ON :</span> ${found.published}</div>
+            <div class="data-item"><span class="label">VALID UNTIL :</span> ${found.valid_until}</div>
+            <div class="data-item"><span class="label">TYPE :</span> ${found.type}</div>
+            <div class="data-item"><span class="label">MODEL :</span> ${found.model}</div>
+            <div class="data-item"><span class="label">COMPANY :</span> ${found.company}</div>
+            <div class="data-item"><span class="label">TRAINING LOCATION :</span> ${found.location}</div>
+            <div class="data-item"><span class="label">TRAINER :</span> ${found.trainer}</div>
         `;
-
     } else {
         alert("Certificate not found");
     }
